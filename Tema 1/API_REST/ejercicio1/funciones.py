f973e14b567e7028d121a07bcdac77198b8e46b5
Fernando <fernandoj.miguel@iesnervion.es>
2023-10-15 20:22:52 +0200

    terminado

--- Tema 1/API_REST/ejercicio1/funciones.py
+++ Tema 1/API_REST/ejercicio1/funciones.py
@@ -8,7 +8,7 @@ def getAllPosts(url):
         for clave in diccionario:
             print (clave, ":", diccionario[clave])
         print()
-#s
+        
 def getPost(url, numPubl):
     response = requests.get(url+"/"+str(numPubl))
     if response.status_code == 200:
@@ -19,3 +19,43 @@ def getPost(url, numPubl):
     else:
         print("La petición no ha terminado correctamente. Código: ", response.status_code)
 
+def addPost(url, idUsuario, id, titlePost, bodyPost):
+    todo = {"userId":idUsuario, "id":id, "title": titlePost, "body":bodyPost}
+    response = requests.post(url, json=todo)
+    if response.status_code == 201:
+        diccionario = response.json()
+        for claves in diccionario:
+            print(claves, ":", diccionario[claves])
+    else:
+        print("La petición no ha terminado correctamente. Código: ", response.status_code)
+
+def modAllPost (url, newUserId, newId, newTitle, newBody):
+    todo = {"userId": newUserId, "id": newId, "title": newTitle, "body": newBody}
+    response = requests.put(url, json=todo)
+    if response.status_code == 200:
+        diccionario = response.json()
+        for claves in diccionario:
+            print(claves, ":", diccionario[claves] )
+
+    else:
+        print("La petición no ha terminado correctamente. Código: ", response.status_code)
+
+def modOnePost (url, campoModificar, nuevoDato):
+    todo = {campoModificar: nuevoDato}
+    response = requests.patch(url, json=todo)
+    if response.status_code == 200:
+        diccionario = response.json()
+        for claves in diccionario:
+            print(claves, ":", diccionario[claves] )
+
+    else:
+        print("La petición no ha terminado correctamente. Código: ", response.status_code)
+
+def delPost (url):
+    response = requests.delete(url)
+    if response.status_code == 200:
+        diccionario = response.json()
+        for claves in diccionario:
+            print(claves, ":", diccionario[claves])
+    else:
+        print("La petición no ha terminado correctamente. Código: ", response.status_code)
--- Tema 1/API_REST/ejercicio1/main.py
+++ Tema 1/API_REST/ejercicio1/main.py
@@ -7,6 +7,9 @@ while opcion!=0:
     print ("1. Mostrar todas las publicaciones")
     print ("2. Mostrar una publicación concreta")
     print ("3. Añadir una nueva publicación")
+    print ("4. Modificar todos los datos de una publicación")
+    print ("5. Modificar un dato concreto de una publicación")
+    print ("6. Eliminar una publicación")
     print ("0. Salir del programa")
 
     opcion = int(input())
@@ -18,3 +21,49 @@ while opcion!=0:
     elif opcion == 2:
         numeroPubli = int (input("Introdice un número de publicación (1-100): "))
         getPost(url, numeroPubli)
+
+    elif opcion == 3:
+        userId = int (input("Introduce el id de usuario: "))
+        id = int (input("Introduce el id: "))
+        title = input("Introduce el título de la publicación: ")
+        body = input("Introduce el cuerpo de la publicación: ")
+        addPost (url, userId, id, title, body)
+
+    elif opcion == 4:
+        idModificar = int (input("Introduce el id del recurso a modificar: "))
+        url = "https://jsonplaceholder.typicode.com/posts/" + str(idModificar)
+        newUserId = int (input("Introduce el nuevo id de usuario: "))
+        newId = int (input("Introduce el nuevo id: "))
+        newTitle = input("Introduce el nuevo título de la publicación: ")
+        newBody = input("Introduce el nuevo cuerpo de la publicación: ")
+        modAllPost(url, newUserId, newId, newTitle, newBody)
+
+    elif opcion == 5:
+        idMod = int (input("Introduce el id del recurso a modificar: "))
+        url = "https://jsonplaceholder.typicode.com/posts/" + str(idMod)
+        opcionCampo = 0
+        print ("1. userId")
+        print ("2. id")
+        print ("3. title")
+        print ("4. body")
+        opcionCampo = int (input("¿Qué desea modificar?: "))
+
+        if opcionCampo == 1:
+            campoModificar = "userId"
+            nuevoDato = int (input("Introduce el nuevo id de usuario: "))
+        elif opcionCampo == 2:
+            campoModificar = "id"
+            nuevoDato = int (input("Introduce el nuevo id: "))
+        elif opcionCampo == 3:
+             campoModificar = "title"
+             nuevoDato = input("Introduce el nuevo título de la publicación: ")
+        elif opcionCampo == 4:
+            campoModificar = "body"
+            nuevoDato = input("Introduce el nuevo cuerpo de la publicación: ")
+
+        modOnePost(url, campoModificar, nuevoDato)
+
+    elif opcion == 6:
+        idEliminar = int (input("Introduce el id del recurso a eliminar: "))
+        url = "https://jsonplaceholder.typicode.com/posts/" + str(idEliminar)
+        delPost (url)
